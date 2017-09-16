137b733f08077a0da1fd9539ba1258582bb1bf4b
Nate Symer <nate@symer.io>
2017-09-16 12:46:52 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# push307
+
+This is the template for a PushGP system for Hamilton's CS 307: Genetic Programming class.
+
+If you choose to make a fork of this to version control your code (which would be a good idea!) please make it private as to not share code with your classmates. Student GitHub users should be able to get free private repositories.
+
+## Usage
+
+There are two ways to run the main PushGP function:
+
+1. Load `core.clj` into the interpreter, and then run `(-main)`.
+2. From the command line, run `lein run`.
+
---
+++ src/push307/core.clj
@@ -0,0 +1,299 @@
+(ns push307.core
+  (:gen-class))
+
+;;;;;;;;;;
+;; Examples
+
+; An example Push state
+(def example-push-state
+  {:exec '(integer_+ integer_-)
+   :integer '(1 2 3 4 5 6 7)
+   :string '("abc" "def")
+   :input {:in1 4 :in2 6}})
+
+; An example Push program
+(def example-push-program
+  '(3 5 integer_* "hello" 4 "world" integer_-))
+
+; An example individual in the population
+; Made of a map containing, at mimimum, a program, the errors for
+; the program, and a total error
+(def example-individual
+  {:program '(3 5 integer_* "hello" 4 "world" integer_-)
+   :errors [8 7 6 5 4 3 2 1 0 1]
+   :total-error 37})
+
+
+;;;;;;;;;;
+;; Instructions must all be either functions that take one Push
+;; state and return another or constant literals.
+(def instructions
+  (list
+   'in1
+   'integer_+
+   'integer_-
+   'integer_*
+   'integer_%
+   0
+   1
+   ))
+
+
+;;;;;;;;;;
+;; Utilities
+
+(def empty-push-state
+  {:exec '()
+   :integer '()
+   :string '()
+   :input {}})
+
+(defn push-to-stack
+  "Pushes item onto stack in state, returning the resulting state."
+  [state stack item]
+  :STUB
+  )
+
+(defn pop-stack
+  "Removes top item of stack, returning the resulting state."
+  [state stack]
+  :STUB
+  )
+
+(defn peek-stack
+  "Returns top item on a stack. If stack is empty, returns :no-stack-item"
+  [state stack]
+  :STUB
+  )
+
+(defn empty-stack?
+  "Returns true if the stack is empty in state."
+  [state stack]
+  :STUB
+  )
+
+(defn get-args-from-stacks
+  "Takes a state and a list of stacks to take args from. If there are enough args
+  on each of the desired stacks, returns a map of the form {:state :args}, where
+  :state is the new state with args popped, and :args is a list of args from
+  the stacks. If there aren't enough args on the stacks, returns :not-enough-args."
+  [state stacks]
+  (loop [state state
+         stacks stacks
+         args '()]
+    (if (empty? stacks)
+      {:state state :args (reverse args)}
+      (let [stack (first stacks)]
+        (if (empty-stack? state stack)
+          :not-enough-args
+          (recur (pop-stack state stack)
+                 (rest stacks)
+                 (conj args (peek-stack state stack))))))))
+
+(defn make-push-instruction
+  "A utility function for making Push instructions. Takes a state, the function
+  to apply to the args, the stacks to take the args from, and the stack to return
+  the result to. Applies the function to the args (taken from the stacks) and pushes
+  the return value onto return-stack in the resulting state."
+  [state function arg-stacks return-stack]
+  (let [args-pop-result (get-args-from-stacks state arg-stacks)]
+    (if (= args-pop-result :not-enough-args)
+      state
+      (let [result (apply function (reverse (:args args-pop-result)))
+            new-state (:state args-pop-result)]
+        (push-to-stack new-state return-stack result)))))
+
+
+;;;;;;;;;;
+;; Instructions
+
+(defn in1
+  "Pushes the input labeled :in1 on the inputs map onto the :exec stack.
+  Can't use make-push-instruction, since :input isn't a stack, but a map."
+  [state]
+  :STUB
+  )
+
+(defn integer_+
+  "Adds the top two integers and leaves result on the integer stack.
+  If integer stack has fewer than two elements, noops."
+  [state]
+  (make-push-instruction state +' [:integer :integer] :integer))
+
+;;;; This is an example of what would be necessary to implement integer_+
+;;;; without the useful helper function make-push-instruction.
+;; (defn integer_+_without_helpers
+;;   [state]
+;;   (if (< (count (:integer state)) 2)
+;;     state
+;;     (let [arg1 (peek-stack state :integer)
+;;           arg2 (peek-stack (pop-stack state :integer) :integer)
+;;           popped-twice (pop-stack (pop-stack state :integer) :integer)]
+;;       (push-to-stack popped-twice :integer (+' arg1 arg2)))))
+
+
+(defn integer_-
+  "Subtracts the top two integers and leaves result on the integer stack.
+  Note: the second integer on the stack should be subtracted from the top integer."
+  [state]
+  :STUB
+  )
+
+(defn integer_*
+  "Multiplies the top two integers and leaves result on the integer stack."
+  [state]
+  :STUB
+  )
+
+(defn integer_%
+  "This instruction implements 'protected division'.
+  In other words, it acts like integer division most of the time, but if the
+  denominator is 0, it returns the numerator, to avoid divide-by-zero errors."
+  [state]
+  :STUB
+  )
+
+
+;;;;;;;;;;
+;; Interpreter
+
+(defn interpret-one-step
+  "Helper function for interpret-push-program.
+  Takes a Push state and executes the next instruction on the exec stack,
+  or if the next element is a literal, pushes it onto the correct stack.
+  Returns the new Push state."
+  [push-state]
+  :STUB
+  )
+
+(defn interpret-push-program
+  "Runs the given program starting with the stacks in start-state. Continues
+  until the exec stack is empty. Returns the state of the stacks after the
+  program finishes executing."
+  [program start-state]
+  :STUB
+  )
+
+
+;;;;;;;;;;
+;; GP
+
+(defn make-random-push-program
+  "Creates and returns a new program. Takes a list of instructions and
+  a maximum initial program size."
+  [instructions max-initial-program-size]
+  :STUB
+  )
+
+(defn tournament-selection
+  "Selects an individual from the population using a tournament. Returned 
+  individual will be a parent in the next generation. Can use a fixed
+  tournament size."
+  [population]
+  :STUB
+  )
+
+(defn crossover
+  "Crosses over two programs (note: not individuals) using uniform crossover.
+  Returns child program."
+  [prog-a prog-b]
+  :STUB
+  )
+
+(defn uniform-addition
+  "Randomly adds new instructions before every instruction (and at the end of
+  the program) with some probability. Returns child program."
+  [prog]
+  :STUB
+  )
+
+(defn uniform-deletion
+  "Randomly deletes instructions from program at some rate. Returns child program."
+  [prog]
+  :STUB
+  )
+
+(defn select-and-vary
+  "Selects parent(s) from population and varies them, returning
+  a child individual (note: not program). Chooses which genetic operator
+  to use probabilistically. Gives 50% chance to crossover,
+  25% to uniform-addition, and 25% to uniform-deletion."
+  [population]
+  :STUB
+  )
+
+(defn report
+  "Reports information on the population each generation. Should look something
+  like the following (should contain all of this info; format however you think
+  looks best; feel free to include other info).
+
+-------------------------------------------------------
+               Report for Generation 3
+-------------------------------------------------------
+Best program: (in1 integer_% integer_* integer_- 0 1 in1 1 integer_* 0 integer_* 1 in1 integer_* integer_- in1 integer_% integer_% 0 integer_+ in1 integer_* integer_- in1 in1 integer_* integer_+ integer_* in1 integer_- integer_* 1 integer_%)
+Best program size: 33
+Best total error: 727
+Best errors: (117 96 77 60 45 32 21 12 5 0 3 4 3 0 5 12 21 32 45 60 77)
+  "
+  [population generation]
+  :STUB
+  )
+
+(defn push-gp
+  "Main GP loop. Initializes the population, and then repeatedly
+  generates and evaluates new populations. Stops if it finds an
+  individual with 0 error (and should return :SUCCESS, or if it
+  exceeds the maximum generations (and should return nil). Should print
+  report each generation.
+  --
+  The only argument should be a map containing the core parameters to
+  push-gp. The format given below will decompose this map into individual
+  arguments. These arguments should include:
+   - population-size
+   - max-generations
+   - error-function
+   - instructions (a list of instructions)
+   - max-initial-program-size (max size of randomly generated programs)"
+  [{:keys [population-size max-generations error-function instructions max-initial-program-size]}]
+  :STUB
+  )
+
+
+;;;;;;;;;;
+;; The functions below are specific to a particular problem.
+;; A different problem would require replacing these functions.
+;; Problem: f(x) = x^3 + x + 3
+
+(defn target-function
+  "Target function: f(x) = x^3 + x + 3
+  Should literally compute this mathematical function."
+  [x]
+  :STUB
+  )
+
+(defn regression-error-function
+  "Takes an individual and evaluates it on some test cases. For each test case,
+  runs program with the input set to :in1 in the :input map part of the Push state.
+  Then, the output is the integer on top of the integer stack in the Push state
+  returned by the interpreter. Computes each error by comparing output of
+  the program to the correct output.
+  Returns the individual with :errors set to the list of errors on each case,
+  and :total-error set to the sum of the errors.
+  Note: You must consider what to do if the program doesn't leave anything
+  on the integer stack."
+  [individual]
+  :STUB
+  )
+
+
+;;;;;;;;;;
+;; The main function. Uses some problem-specific functions.
+
+(defn -main
+  "Runs push-gp, giving it a map of arguments."
+  [& args]
+  (push-gp {:instructions instructions
+            :error-function regression-error-function
+            :max-generations 500
+            :population-size 200
+            :max-initial-program-size 50}))
