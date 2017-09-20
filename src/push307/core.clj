ad1f4272764d9bd8acf24f0367bb5ab67f64b504
jackHay22 <jjphay@gmail.com>
2017-09-20 19:33:11 -0400

    add macro for instruction definition

--- src/push307/core.clj
+++ src/push307/core.clj
@@ -11,8 +11,11 @@
 (defn -main
   "Runs push-gp, giving it a map of arguments."
   [& args]
+ 
   (push-gp {:instructions instructions
             :error-function regression-error-function
             :max-generations 500
             :population-size 200
-            :max-initial-program-size 50}))
+            :max-initial-program-size 50
+     
+            }))
--- src/push307/push/instructions.clj
+++ src/push307/push/instructions.clj
@@ -1,54 +1,59 @@
 (ns push307.push.instructions
   (:gen-class))
 
-(defn in1
+(def in1
   "Pushes the input labeled :in1 on the inputs map onto the :exec stack.
   Can't use make-push-instruction, since :input isn't a stack, but a map."
-  [state]
-  (cons (:in1 (:input state)) (:exec state))
-  )
+  (fn [state] (cons (:in1 (:input state)) (:exec state))))
 
-; (defn makeInstruction
-;   [stack]
-; )
+(def in2
+  "Pushes the input labeled :in1 on the inputs map onto the :exec stack.
+  Can't use make-push-instruction, since :input isn't a stack, but a map."
+  (fn [state] (cons (:in2 (:input state)) (:exec state))))
 
-(defn integer_+
+
+
+;define instructions wil macro
+(defmacro make-instruction 
+  "make instructions"
+  [name operation outputstack in1 in2]
+  (list 'def name (list 'fn (vector 'state) 
+                  (list 'make-push-instruction 'state operation 
+                  (vector in1 in2)  outputstack))))
+
+
+(def instruction-sets
+  '(integer_+ +' :integer :integer :integer))
+
+;map make-instruction across instruction-sets
+
+(def integer_+
   "Adds the top two integers and leaves result on the integer stack.
   If integer stack has fewer than two elements, noops."
-  [state]
-  (make-push-instruction state +' [:integer :integer] :integer))
+  (fn [state](make-push-instruction state +' [:integer :integer] :integer)))
 
-
-(defn integer_-
+ 
+(def integer_-
   "Subtracts the top two integers and leaves result on the integer stack.
   Note: the second integer on the stack should be subtracted from the top integer."
-  [state]
-  (make-push-instruction state -' [:integer :integer] :integer))
+  (fn [state] (make-push-instruction state -' [:integer :integer] :integer)))
 
 (defn integer_*
   "Multiplies the top two integers and leaves result on the integer stack."
-  [state]
-  (make-push-instruction state *' [:integer :integer] :integer))
-  )
-
+  (fn [state] (make-push-instruction state *' [:integer :integer] :integer))))
+ 
 (defn integer_%
   "This instruction implements 'protected division'.
   In other words, it acts like integer division most of the time, but if the
   denominator is 0, it returns the numerator, to avoid divide-by-zero errors."
-  [state]
-  (make-push-instruction state (fn [x y] (if (= 0 y) x (/ x y))) [:integer :integer] :integer))
-  )
+  (fn [state] (make-push-instruction state (fn [x y] (if (= 0 y) x (/ x y))) [:integer :integer] :integer))))
 
 (defn integer_>
   "Pushes true onto the boolean stack if the second
   item is greater than the top item, else false"
-  [state]
-  (make-push-instruction state (fn [x y] (> y x)) [:integer :integer] :boolean))
-  )
+  (fn [state] (make-push-instruction state (fn [x y] (> y x)) [:integer :integer] :boolean))))
 
 (defn integer_<
   "Pushes true onto the boolean stack if the second
   item is less than the top item, else false."
-  [state]
-  (make-push-instruction state (fn [x y] (< y x)) [:integer :integer] :boolean))
-  )
+  (fn [state] (make-push-instruction state (fn [x y] (< y x)) [:integer :integer] :boolean))))
