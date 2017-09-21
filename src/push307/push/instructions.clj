4fa8b1a3a4f0fd3359a484195f088cb25459e741
Nate Symer <nate@symer.io>
2017-09-20 22:39:34 -0400

    action item FIXME

--- src/push307/push/instructions.clj
+++ src/push307/push/instructions.clj
@@ -12,6 +12,15 @@
   Can't use make-push-instruction, since :input isn't a stack, but a map."
   (fn [state] (cons (:in2 (:input state)) (:exec state))))
 
+;; TODO: enable this to push to multiple stacks.
+;;  e.g. vectors/lists -> push multiple
+;;       dictionaries -> push multiple on different stacks
+;; You could redefine the meaning of these things as applied
+;; here because you couldn't enforce type safety when pushing
+;; dicts and lists to stacks, so Push wouldn't care if we
+;; commandeered these types.
+;; This would be needed to be abstract enough while
+;; being able to implement  exec_* instructions & in*.
 (defmacro definstr
   "Macro for defining Push instructions."
   [name arg-stacks outputstack operation]
@@ -19,10 +28,24 @@
                   (list 'make-push-instruction 'state operation 
                   arg-stacks outputstack))))
 
-;(def instruction-sets
-;  '(integer_+ +' :integer :integer :integer))
-
-;map definstr across instruction-sets
+;; FIXME: make-push-instruction returns args backwards
+;;        and this causes definstr's "typed function declaration"
+;;        syntax to be misleading.
+;; Flipping the order would fix integer_> and friends.
+;; This makes more sense because it upholds the notational
+;; order of Push.
+;; 
+;; This is proven by
+;; ... define make-push-instruction & definstr
+;; (definstr i [:integer :boolean] :exec (fn [i b] [i b]))
+;; => #'user/i
+;; (i { :integer '(1 2 3) :boolean '(true) })
+;; => {:integer (2 3), :boolean (), :exec ([true 1])}
+;; Options:
+;; 1. s/arg-stacks/(reverse arg-stacks)/ on line 29.
+;; 2. patch make-push-instruction
+;;
+;; Which course of action should be taken? I prefer option 1.
 
 (definstr integer_+ [:integer :integer] :integer +')
 (definstr integer_- [:integer :integer] :integer -')
