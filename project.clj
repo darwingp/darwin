eaa2e564feef64f9b8e84267345686a41b5be5e8
jackHay22 <jjphay@gmail.com>
2017-09-28 23:48:37 -0400

    generators dependency

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/test.generative "0.5.2"]]
+                 [org.clojure/test.generative "0.5.2"]
+                 [org.clojure/data.generators "0.1.2"]]
   :main ^:skip-aot push307.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
