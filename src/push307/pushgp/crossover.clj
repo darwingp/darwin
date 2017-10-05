9c59b79e190f77a3bb7b91e294fa893f80ee0661
jackHay22 <jjphay@gmail.com>
2017-10-05 16:17:23 -0400

    more testing, recorded average individual growth for ac

--- src/push307/pushgp/crossover.clj
+++ src/push307/pushgp/crossover.clj
@@ -23,6 +23,16 @@
         (Math/cos (* 2 Math/PI v))))
   )))
 
+
+;NOTES:
+;-------------------
+; Testing alternation crossover 1000 times with prog sizes of 30, 
+;0.2 alternation rate, 4 alignment deviation yielded an average size of 48.
+; Code:
+; (/ (reduce + (repeatedly 10000 #(count (alternation-crossover (range 40) (range 40) 0.2 4)))) 10000)
+; This dropped to 45 with 0.3, 3
+; I will test it more to see how we can mitigate bloat...
+
 (defn alternation-crossover
   "Crosses over two programs (note: not individuals) using alternation crossover
   takes alternation rate and alignment-deviation"
