444a6668971873ef4833bcaca2446e29ee4046aa
jackHay22 <jjphay@gmail.com>
2017-10-11 15:49:03 -0400

    refactor initial machine function

--- src/push307/testmachine/machine.clj
+++ src/push307/testmachine/machine.clj
@@ -1,22 +1,20 @@
 (ns push307.testmachine.machine (:gen-class))
 
+(def standard-start '(0 0))
+
 (def move-dxdy (fn [x y dx dy] '((+ x dx) (+ y dy))))
 
 (defn new-move
   "take obstacle-list, vehicle loc, new move, returns new loc based on move/obstacles and angle"
-  [obstacles vehicle move angle]
-  vehicle
-)
+  [obstacles]
+  ;lambda takes pair: current-loc and instruction
+  (fn [loc instr]
+    ;return new loc based on instruction
+    ))
 
 (defn test-instructions
   "takes in a list of vehicle instructions, a list of obstacles
   outputs a map of fitness (can be used for behavioral tracking too)"
   [instructionlist obstaclelist]
-    (loop [vehicle '(0 0 0)  ;x y angle
-           rem-instr instructionlist
-           result-state {:dist-to-target 500 :num-crash 0 :instr-rem (count instructionlist)}]
-      (if (= rem-instr '()) result-state
-        (recur
-            (new-move obstaclelist vehicle (first rem-instr))
-            (rest rem-instr)
-            result-state))))
+    (let [result {:dist-to-target 500 :num-crash 0 :instr-rem (count instructionlist)}]
+    (reduce (new-move obstaclelist) standard-start instructionlist)))
