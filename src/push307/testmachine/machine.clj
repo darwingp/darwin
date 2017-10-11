ad7eca3a917019bc2ed091cb8d8ac80c03a4dd33
jackHay22 <jjphay@gmail.com>
2017-10-11 16:24:06 -0400

    add machine functions

--- src/push307/testmachine/machine.clj
+++ src/push307/testmachine/machine.clj
@@ -1,20 +1,48 @@
 (ns push307.testmachine.machine (:gen-class))
 
-(def standard-start '(0 0))
+(def start-loc '(0 0 0))   ;x y angle
+(def target-loc '(200 50))
+(def move-length 1)
 
-(def move-dxdy (fn [x y dx dy] '((+ x dx) (+ y dy))))
+(def move
+  "move based on angle and x,y"
+  (fn [location]
+    (let [x (first location) y (second location) angle (nth location 2)]
+      (list
+        ;Note: currently expects radians
+        (+ x (* move-length (Math/cos angle)))
+        (+ y (* move-length (Math/sin angle)))
+        ))))
+
+(defn change-angle
+  "takes instruction, if angle-based, modifies angle val"
+  [loc instr]
+  (if (= (first instr) :angle)
+    (concat (take 2 loc) [(second instr)])
+    loc
+))
+
+(def distance
+  "calculate distance between points"
+  (fn [x1 y1 x2 y2]
+    (let [xdif (- x2 x1) ydif (- y2 y1)]
+    (Math/sqrt (+ (* xdif xdif) (* ydif ydif)))
+)))
 
 (defn new-move
   "take obstacle-list, vehicle loc, new move, returns new loc based on move/obstacles and angle"
   [obstacles]
-  ;lambda takes pair: current-loc and instruction
+  ;lambda takes pair: current-loc (x y angle) and instruction
   (fn [loc instr]
     ;return new loc based on instruction
+    (move (change-angle loc instr))
     ))
 
 (defn test-instructions
   "takes in a list of vehicle instructions, a list of obstacles
   outputs a map of fitness (can be used for behavioral tracking too)"
   [instructionlist obstaclelist]
-    (let [result {:dist-to-target 500 :num-crash 0 :instr-rem (count instructionlist)}]
-    (reduce (new-move obstaclelist) standard-start instructionlist)))
+    (let [final-loc (reduce (new-move obstaclelist) start-loc instructionlist)]
+    {:dist-to-target (distance (first final-loc) (second final-loc) (first target-loc) (second target-loc))
+     :num-crash 0
+     :instr-rem (count instructionlist)}))
